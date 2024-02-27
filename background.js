cd505ca36b74ab1b61f8a800e40b16be270f6516
Dmitriy Vasilev <raoffonom@icloud.com>
2024-02-27 19:40:28 +0700

    transcript

---
+++ background.js
@@ -0,0 +1,87 @@
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log(message.type)
+  if (message.type == "save_and_download") {
+    chrome.storage.local.set(
+      {
+        transcript: message.transcript,
+        meetingTitle: message.meetingTitle,
+        meetingStartTimeStamp: message.meetingStartTimeStamp
+      },
+      function () {
+        console.log(
+          "Saved transcript and meta data, downloading now if non empty"
+        )
+        if (message.transcript.length > 0) downloadTranscript()
+      }
+    )
+  }
+  if (message.type == "download") {
+    downloadTranscript()
+  }
+  return true
+})
+
+function downloadTranscript() {
+  chrome.storage.local.get(
+    ["transcript", "meetingTitle", "meetingStartTimeStamp"],
+    function (result) {
+      if (result.transcript) {
+        const fileName =
+          result.meetingTitle && result.meetingStartTimeStamp
+            ? `Srumy/Transcript-${result.meetingTitle} at ${result.meetingStartTimeStamp}.txt`
+            : `Srumy/Transcript.txt`
+
+        // Create an array to store lines of the text file
+        const lines = []
+
+        // Iterate through the transcript array and format each entry
+        result.transcript.forEach((entry) => {
+          lines.push(entry.personName)
+          lines.push(entry.personTranscript)
+          lines.push("") // Add an empty line between entries
+        })
+
+        lines.push("---")
+        lines.push(
+          "Transcript saved using Srumy Chrome extension (https://chromewebstore.google.com/detail/ciepnfnceimjehngolkijpnbappkkiag)"
+        )
+
+        // Join the lines into a single string
+        const textContent = lines.join("\n")
+
+        // Create a Blob from the text content
+        const blob = new Blob([textContent], { type: "text/plain" })
+
+        // Create a download
+        // Use Chrome Download API
+        chrome.downloads
+          .download({
+            url: "data:text/plain;base64," + encodeUnicodeString(textContent),
+            filename: fileName,
+            conflictAction: "uniquify" // Automatically rename the file if it already exists
+          })
+          .then(() => {
+            console.log("Transcript downloaded to Srumy directory")
+          })
+          .catch((error) => {
+            console.log(error)
+            chrome.downloads.download({
+              url: "data:text/plain;base64," + encodeUnicodeString(textContent),
+              filename: "Srumy/Transcript.txt",
+              conflictAction: "uniquify" // Automatically rename the file if it already exists
+            })
+            console.log(
+              "Invalid file name. Transcript downloaded to Srumy directory with simple file name."
+            )
+          })
+      } else console.log("No transcript found")
+    }
+  )
+}
+
+// Thanks to @ifTNT(https://github.com/vivek-nexus/Srumy/pull/4)
+function encodeUnicodeString(text) {
+  const utf8Bytes = new TextEncoder().encode(text)
+  const binaryString = String.fromCodePoint(...utf8Bytes)
+  return btoa(binaryString)
+}
